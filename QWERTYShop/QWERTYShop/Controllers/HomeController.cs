c263bccac5f9465cee6aae8924b23fa69eb4193e
VayFull <40316778+VayFull@users.noreply.github.com>
2019-10-20 15:33:23 +0300

    Изменение города
    Изменение города

--- QWERTYShop/QWERTYShop/Controllers/HomeController.cs
+++ QWERTYShop/QWERTYShop/Controllers/HomeController.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using QWERTYShop.Models;
 
 namespace QWERTYShop.Controllers
 {
     public class HomeController : Controller
     {
+        string ConnectionString = "Server = localhost; Port=5432; Database=postgres; User Id =postgres; Password=1234QWER+";
         public ActionResult Index()
         {
             return View();
@@ -28,9 +31,53 @@ namespace QWERTYShop.Controllers
 
             return View();
         }
-
+        
+        [HttpGet]
         public ActionResult ChangeCity()
         {
+            List<string> Data = new List<string>();
+            using (var connection = new NpgsqlConnection(ConnectionString))
+            {
+                connection.Open();
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT City FROM public.CityList;", connection))
+                {
+                    var reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Data.Add(reader.GetString(0));
+                        }
+                    }
+                }
+                ViewBag.CityList = Data;
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ChangeCity(CityModels model)
+        {
+            Session["CityName"] = model.City;
+            ViewBag.ChangeCitySuccess = "Успешно!";
+            List<string> Data = new List<string>();
+            using (var connection = new NpgsqlConnection(ConnectionString))
+            {
+                connection.Open();
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT City FROM public.CityList;", connection))
+                {
+                    var reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Data.Add(reader.GetString(0));
+                        }
+                    }
+                }
+                ViewBag.CityList = Data;
+                return View();
+            }
             return View();
         }
     }
