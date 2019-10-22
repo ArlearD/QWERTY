272a0a245cd17c80a9a0d482b3e475e2c8a015f6
VayFull <40316778+VayFull@users.noreply.github.com>
2019-10-22 21:49:04 +0300

    Оформление покупки v0.8

---
+++ QWERTYShop/QWERTYShop/Models/PaymentModels.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QWERTYShop.Models
+{
+    public class PaymentModels
+    {
+        [Display(Name = "Номер карты")]
+        [Required(ErrorMessage = "Введите номер карты")]
+        public string CardNumber { get; set; }
+
+        [Display(Name = "CVC")]
+        [Required(ErrorMessage = "Введите CVC")]
+        public string CVC { get; set; }
+
+        [Display(Name = "Месяц/год до которого доступна карта")]
+        [Required(ErrorMessage = "Введите месяц и год")]
+        public string DataValidation { get; set; }
+
+        [Display(Name = "Название фамилию и имя указанные на карте")]
+        [Required(ErrorMessage = "Заполните это поле!")]
+        public string Person { get; set; }
+    }
+}
