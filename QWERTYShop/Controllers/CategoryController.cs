fd66f4843a264390c159f2233dcb22e459c7ae81
VayFull <40316778+VayFull@users.noreply.github.com>
2019-11-24 23:56:47 +0300

    фильтрация по цене и рейтингу

--- QWERTYShop/Controllers/CategoryController.cs
+++ QWERTYShop/Controllers/CategoryController.cs
@@ -14,6 +14,8 @@ namespace QWERTYShop.Controllers
         public ActionResult Category(string category)
         {
             GetCategories(category);
+            var maxPrice = GetMaxPrice(category);
+            ViewBag.Values = new float[] { 0, maxPrice, 0, 10 };
             ViewBag.Sorted = "По рейтингу";
             return View();
         }
@@ -22,9 +24,16 @@ namespace QWERTYShop.Controllers
         [Route("category/{category}")]
         public ActionResult Category(string category, CategoryModels model) //method
         {
+            GetFilteringValues(category, model);
             GetSortedCategories(category, model);
+            ViewBag.Max = GetMaxPrice(category);
             ViewBag.Sorted = model.Method;
             return View();
+        }
+
+        private void GetFilteringValues(string category, CategoryModels model)
+        {
+            ViewBag.Values = new float[] {model.FromPrice, model.ToPrice, model.FromRating, model.ToRating};
         }
 
         private void GetCategories(string category)
@@ -74,7 +83,8 @@ namespace QWERTYShop.Controllers
             {
                 connection.Open();
                 using (var command =
-                    new NpgsqlCommand($"select * from cards where type='{category}' order by {orderType} {typeOfSort}",
+                    new NpgsqlCommand($"select * from cards where type='{category}' and cost>={model.FromPrice} and cost<={model.ToPrice} " +
+                                      $" and averagemark>={model.FromRating} and averagemark<={model.ToRating} order by {orderType} {typeOfSort}",
                         connection))
                 {
                     var reader = command.ExecuteReader();
@@ -97,9 +107,26 @@ namespace QWERTYShop.Controllers
             }
         }
 
-        private void GetFilters(string category)
+        private float GetMaxPrice(string category)
         {
+            float value = 0;
+            using (var connection = new NpgsqlConnection(ConnectionString))
+            {
+                connection.Open();
+                var cmd = new NpgsqlCommand($"select max(cost) from cards where type='{category}' and cost is not null", connection);
+                var reader = cmd.ExecuteReader();
 
+                while (reader.Read()) 
+                {
+                    if (!reader.IsDBNull(0)) 
+                    {
+                        value = reader.GetInt32(0); //сделать "возврат" значений 
+                    }
+                }
+                connection.Close();
+            }
+
+            return value;
         }
     }
 }
--- QWERTYShop/Models/CategoryModels.cs
+++ QWERTYShop/Models/CategoryModels.cs
@@ -5,5 +5,9 @@
         public string Type { get; set; }
         public string Method { get; set; }
         public bool IsDescending { get; set; }
+        public int FromPrice { get; set; }
+        public int ToPrice { get; set; }
+        public int FromRating { get; set; }
+        public int ToRating { get; set; }
     }
 }
